2576c8a62270f043637d63ae9ef8291c504f625e
JainPriya1234 <priya.jain11302@gmail.com>
2024-10-13 20:15:38 +0530

    added proj

---
+++ Data/RazorPageMovieeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMoviee.Models;
+
+namespace RazorPageMoviee.Data
+{
+    public class RazorPageMovieeContext : DbContext
+    {
+        public RazorPageMovieeContext (DbContextOptions<RazorPageMovieeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPageMoviee.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPageMoviee.Data;
+using RazorPageMoviee.Models;
+namespace RazorPageMoviee
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<RazorPageMovieeContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPageMovieeContext") ?? throw new InvalidOperationException("Connection string 'RazorPageMovieeContext' not found.")));
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                SeedData.Initialize(services);
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
